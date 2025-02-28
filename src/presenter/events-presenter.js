b6e1167bd4d354312fef1bb46ba92860e46c49a2
cooperfirewalk <cooperfirewalk@gmail.com>
2025-02-28 11:57:05 +0300

    выделяет показ задач в функцию в презентере

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,6 +1,6 @@
 import TripEventsListView from '../view/trip-events-list-view.js';
 import TripSortView from '../view/trip-sort-view.js';
-import EditPointView from '../view/edit-point-view.js';
+// import EditPointView from '../view/edit-point-view.js';
 import PointView from '../view/point-view.js';
 import { render } from '../framework/render.js';
 
@@ -26,10 +26,15 @@ export default class EventsPresenter {
 
     render(new TripSortView(), this.#eventsContainer);
     render(this.#listComponent, this.#eventsContainer);
-    render(new EditPointView({ point: this.#eventsPoints[0], offers: this.#offers, destinations: this.#destinations }), this.#listComponent.element);
 
-    for (let i = 1; i < 4; i++) {
-      render(new PointView({ point: this.#eventsPoints[i], offers: this.#offers, destinations: this.#destinations }), this.#listComponent.element);
+    for (let i = 0; i < 3; i++) {
+      this.#renderPoint(this.#eventsPoints[i]);
     }
+  }
+
+  #renderPoint(point) {
+    const pointComponent = new PointView({ point, offers: this.#offers, destinations: this.#destinations });
+
+    render(pointComponent, this.#listComponent.element);
   }
 }
