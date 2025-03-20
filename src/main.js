f4537d56de500f071c419c8fa98ecd3f14b465eb
cooperfirewalk <cooperfirewalk@gmail.com>
2025-03-20 15:14:39 +0300

    Добавит модель для фильтра. WIP

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import {render} from './framework/render.js';
 import FiltersView from './view/filters-view.js';
 import EventsPresenter from './presenter/events-presenter.js';
 import EventsModel from './model/events-model.js';
+import FilterModel from './model/filter-model.js';
 import {generateFilter} from './mock/filter.js';
 
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
 const eventsModel = new EventsModel();
+const filterModel = new FilterModel();
 const eventsPresenter = new EventsPresenter({
   eventsContainer: tripEventsContainer,
   eventsModel
