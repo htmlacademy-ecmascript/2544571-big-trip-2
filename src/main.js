3ba98d87c3c16a3607b163551d8969e098261a0d
cooperfirewalk <cooperfirewalk@gmail.com>
2025-03-21 09:46:05 +0300

    добавляет компоненты кнопки и информации о поездке в хедере

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-// import {render} from './framework/render.js'; //для отрисовки кнопки новой задачи
+import {render} from './framework/render.js';
+import { RenderPosition } from './framework/render.js';
+import NewPointButtonView from './view/new-point-button-view.js';
+import TripInfoView from './view/trip-info-view.js';
 import EventsPresenter from './presenter/events-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import EventsModel from './model/events-model.js';
 import FilterModel from './model/filter-model.js';
 
-const filtersContainer = document.querySelector('.trip-controls__filters');
+const siteHeaderElement = document.querySelector('.trip-main');
+const filtersContainer = siteHeaderElement.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
 const eventsModel = new EventsModel();
 const filterModel = new FilterModel();
@@ -19,7 +23,8 @@ const filterPresenter = new FilterPresenter({
   eventsModel
 });
 
-// в демо проекте здесь отрисовка кнопки новой задачи
+render(new TripInfoView(), siteHeaderElement, RenderPosition.AFTERBEGIN); // элемент для доп задания
+render(new NewPointButtonView(), siteHeaderElement);
 
 filterPresenter.init();
 eventsPresenter.init();
