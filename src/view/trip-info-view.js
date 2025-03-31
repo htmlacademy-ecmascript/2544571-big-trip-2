ba905b75a9e04669ad7490344b6da6cf24f50688
cooperfirewalk <cooperfirewalk@gmail.com>
2025-03-31 20:20:37 +0300

    добавляет логику подсчета итоговой суммы

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -32,6 +32,12 @@ function createNewTripInfoTemplate(eventsModel) {
   const humanizedStartDate = isMonthYearEqual(startDate, endDate) ? dayjs(startDate).format('D') : dayjs(startDate).format('D MMM');
   const humanizedEndDate = dayjs(endDate).format('D MMM');
 
+  const totalSum = sortedPoints.reduce((accumulator, point) => {
+    const allOffers = eventsModel.offers;
+    const pointTypeOffers = allOffers.find((offer) => offer.type === point.type).offers;
+    return accumulator + point.basePrice + point.offers.reduce((innerAccumulator, offerId) => innerAccumulator + pointTypeOffers.find((offer) => offer.id === offerId).price, 0);
+  }, 0);
+
   return `  <section class="trip-main__trip-info  trip-info">
             <div class="trip-info__main">
               <h1 class="trip-info__title">${firstPointName} ${secondPointName} ${dots} ${lastPointName} </h1>
@@ -40,7 +46,7 @@ function createNewTripInfoTemplate(eventsModel) {
             </div>
 
             <p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalSum}</span>
             </p>
           </section>`;
 }
